ebc39ae34fab377d5115f550928e7a61a1ab57e7
cjkarinen20 <cjkarinen20@gmail.com>
2025-11-02 12:50:14 -0500

    Created an example file

---
+++ C-Compiler/return_2c.cs
@@ -0,0 +1,13 @@
+using System;
+
+class return_2C
+{
+    public static void Main(string[] args)
+    {
+        return2();
+    }
+    public static int return2()
+    {
+        return 2;
+    }
+}
